4a5fddec107001d43ceaeaa989cb47afde960ff0
isita <isita.bagayatkar@gmail.com>
2023-05-02 11:25:01 -0700

    did one more

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def getMainMenuAndInput():
     \n\t 3 Get articles by author and political ideology leaning \
     \n\t 4 Get articles by wordcount \
     \n\t 5 Get articles by key terms \
-    \n\t 6 Get article headlines by year \
+    \n\t 6 Get article Investigative headlines by year \
     \n\t 7 Get articles by organization and get polical ideology leaning \
     \n\t 8 Get this week's top articles \
     \n\t 9 Get top articles by topic")
@@ -74,6 +74,12 @@ def case5():
   for article in articles:
     print(article)
 
+def case6():
+  articles = articles_collection.find({'News Desk': 'Investigative'}, {'_id':0, 'Headline':1, 'Year': 1}).sort('Year',-1).limit(1000)
+  # clean this up by year
+  for article in articles:
+    print(article)
+
 def case16():
   headlines = articles_collection.find({'Year': {'$gte': 2022}}, {'_id':0, 'Headline':1}).limit(10)
   for headline in headlines:
@@ -100,6 +106,8 @@ def main():
       case4()
     elif user_input == 5:
       case5()
+    elif user_input == 6:
+      case6()
     elif user_input == 16:
       case16()
     elif user_input == 17:
