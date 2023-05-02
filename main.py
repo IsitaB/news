e941dba46d03dcb1e657ab25cffc987b6825d7d2
isita <isita.bagayatkar@gmail.com>
2023-05-02 11:10:07 -0700

    need to format output better

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import os
 load_dotenv()
 MONGO_URI = os.environ['MONGO_URI']
 
+'''Connect to mongodb'''
+
+client = MongoClient()
+client = MongoClient(MONGO_URI)
+
+db = client['nytdb']
+articles_collection = db['articles']
+print(db.list_collection_names())
+
 def getMainMenuAndInput():
   print('Welcome to the NYT Analysis Dashboard \n')
 
@@ -15,7 +24,7 @@ def getMainMenuAndInput():
     \n\t 2 Get articles by topic and political ideology leaning \
     \n\t 3 Get articles by author and political ideology leaning \
     \n\t 4 Get articles by wordcount \
-    \n\t 5 Get articles by headline key terms \
+    \n\t 5 Get articles by key terms \
     \n\t 6 Get article headlines by year \
     \n\t 7 Get articles by organization and get polical ideology leaning \
     \n\t 8 Get this week's top articles \
@@ -35,33 +44,66 @@ def getMainMenuAndInput():
   print("\n\nMight help you with your search\n \
     \n\t 17 Get all topics \
     \n\t 18 Get the org and authors breakdown")
+  
+  print("\n\nAdmin\n \
+    \n\t 19 Delete article \
+    \n\t 20 Update article \
+    \n\t 21 Insert dummy article")
 
   return input('\n\n\nEnter the number of a menu option or Q to exit \n')
 
 
 def case1():
-  print('one')
+  topic = input('Please enter the topic of interest: ')
+  year = input('Which year: ')
+  month = input('Lastly, in which month? ')
+
+  res = articles_collection.find({"$and": [{'News Desk': {"$in": [topic]}}, {'Year':year, 'Month': month}]})
+  for article in res:
+    print(article)
+
+def case4():
+  wordLimit = int(input("I get that reading takes a while. We'll suggest 10 articles within your word limit. What's your word limit for articles? "))
+  articles = articles_collection.find({'Word Count': {"$lte": wordLimit}}).limit(10)
+  for article in articles:
+    print(article)
+
+def case5():
+  keyword = input("Give us a keyword you want to read about: ")
+  articles = articles_collection.find({"$and": [{'Keywords': {"$regex": keyword}}, {'Year': {'$gte': 2022}}]}).limit(10)
+  for article in articles:
+    print(article)
+
+def case16():
+  headlines = articles_collection.find({'Year': {'$gte': 2022}}, {'_id':0, 'Headline':1}).limit(10)
+  for headline in headlines:
+    print(headline)
+  # throw in clickbait detection model
+
+def case17():
+  topics = articles_collection.distinct('News Desk')
+  print(topics)
 
 
 def main():
-  '''Connect to mongodb'''
-
-  client = MongoClient()
-  client = MongoClient(MONGO_URI)
-
-  db = client['nytdb']
-  articles_collection = db['articles']
-  print(db.list_collection_names())
-
   user_input = ''
   while user_input != 'Q':
     user_input = getMainMenuAndInput()
+    user_input = int(user_input)
     # throw in some input validation here
-    if user_input == 'Q':
+    if user_input == '00':
       print('Byeee')
       break
-    if int(user_input) == 1:
+    elif user_input == 1:
       case1()
+    elif user_input == 4:
+      case4()
+    elif user_input == 5:
+      case5()
+    elif user_input == 16:
+      case16()
+    elif user_input == 17:
+      case17()
 
     break # for testing
 
