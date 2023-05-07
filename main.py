19f5f34b95931c3ee2bf5ee5630ef02090b85249
isita <isita.bagayatkar@gmail.com>
2023-05-06 20:10:03 -0700

    uncommenting case16

--- main.py
+++ main.py
@@ -13,9 +13,8 @@ import re
 
 from dotenv import load_dotenv
 import os
-# import pickle
+import pickle5 as pickle
 
-print("hello")
 load_dotenv()
 MONGO_URI = os.environ['MONGO_URI']
 
@@ -28,8 +27,8 @@ db = client['nytdb']
 articles_collection = db['articles']
 print(db.list_collection_names())
 '''Load clickbait model'''
-# tfidf_reloaded = pickle.load(open('tfidf.pkl', "rb"))
-# model_reloaded = pickle.load(open('clickbaitmodel.pkl', "rb"))
+tfidf_reloaded = pickle.load(open('tfidf.pkl', "rb"))
+model_reloaded = pickle.load(open('clickbaitmodel.pkl', "rb"))
 def getMainMenuAndInput():
   print('Welcome to the NYT Analysis Dashboard \n')
 
@@ -167,18 +166,18 @@ def case9():
     for i, article in enumerate(articles):
         print(f"{i+1}. {article['Headline']}")       
 
-# def case16():
-#   tfidf_reloaded = pickle.load(open('tfidf.pkl', "rb"))
-#   model_reloaded = pickle.load(open('clickbaitmodel.pkl', "rb"))
+def case16():
+  tfidf_reloaded = pickle.load(open('tfidf.pkl', "rb"))
+  model_reloaded = pickle.load(open('clickbaitmodel.pkl', "rb"))
 
-#   res_df = pd.DataFrame(list(articles_collection.find({'Year': {'$gte': 2022}}, {'_id':0, 'Headline':1}).limit(10)))
-#   tfidf_res_text = tfidf_reloaded.transform(res_df['Headline'])
-#   preds = model_reloaded.predict(tfidf_res_text)
+  res_df = pd.DataFrame(list(articles_collection.find({'Year': {'$gte': 2022}}, {'_id':0, 'Headline':1}).limit(10)))
+  tfidf_res_text = tfidf_reloaded.transform(res_df['Headline'])
+  preds = model_reloaded.predict(tfidf_res_text)
 
-#   result = np.where(preds == 0, 'Not Clickbait', 'Clickbait')
+  result = np.where(preds == 0, 'Not Clickbait', 'Clickbait')
 
-#   res_df['Clickbait'] = result.tolist()
-#   print(res_df.to_markdown())
+  res_df['Clickbait'] = result.tolist()
+  print(res_df.to_markdown())
 
 def case17():
   topics = pd.DataFrame(list(articles_collection.aggregate([{'$group': {'_id': '$News Desk', 'count': {'$sum':1}}}])))
@@ -258,9 +257,7 @@ def main():
     elif user_input == 20:
       case20()
     elif user_input == 21:
-      case21()  
-
-    break # for testing
+      case21() 
 
 
 main()
